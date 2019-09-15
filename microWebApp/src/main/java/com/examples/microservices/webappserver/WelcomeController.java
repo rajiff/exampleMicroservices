5cb576468901ccd0208d52d92101d02d0ef3ae78
Basavaraj K N <Basavaraj.K@Stackroute.in>
2019-09-15 13:48:35 +0530

    added angular app and able to run it with spring boot, though live reload is still not worked on

--- microWebApp/src/main/java/com/examples/microservices/webappserver/WelcomeController.java
+++ microWebApp/src/main/java/com/examples/microservices/webappserver/WelcomeController.java
@@ -1,18 +1,6 @@
 package com.examples.microservices.webappserver;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class WelcomeController {
-		
-    @GetMapping("")
-    public String welcome() {
-        return "index"; // return index.html as response
-    }
-    
-    @GetMapping("/error")
-    public String error() {
-        return "error";
-    }
-}
+public class WelcomeController { }
