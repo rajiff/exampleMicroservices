d84c5136bc66a0c45586fec3da96538ad67cd6d5
Basavaraj K N <Basavaraj.K@Stackroute.in>
2019-09-16 11:39:47 +0530

    refactored dockerization to include only the built as final image

--- microWebApp/src/main/java/com/examples/microservices/webappserver/WelcomeController.java
+++ microWebApp/src/main/java/com/examples/microservices/webappserver/WelcomeController.java
@@ -3,4 +3,6 @@ package com.examples.microservices.webappserver;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class WelcomeController { }
+public class WelcomeController {
+	// Nothing as of now, just using it for static content 
+}
